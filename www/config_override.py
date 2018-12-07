38a4a1cd70283286bb23a94dcfff37bb33b15d56
Dante-Go <kafei_20123@163.com>
2018-12-07 09:35:14 +0800

    add config_override.py

---
+++ www/config_override.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+##############################################################
+# Copyright (C)2018 Liu Kai. All rights reserved
+# Filename: config_override.py
+# Author: LiuKai
+# Description: 
+# 
+##############################################################
+
+configs = {
+	'db': {
+		'host': '192.168.159.134'
+	}
+}
