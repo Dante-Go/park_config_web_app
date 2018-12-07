26a44e5fb3f7904798f2c99c0a538837e0421d78
Dante-Go <kafei_20123@163.com>
2018-12-07 09:33:51 +0800

    add config_default.py

---
+++ www/config_default.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+##############################################################
+# Copyright (C)2018 Liu Kai. All rights reserved
+# Filename: config_default.py
+# Author: LiuKai
+# Description: 
+# 
+##############################################################
+
+configs = {
+	'debug': True,
+	'db':{
+		'host': '127.0.0.1',
+		'port': 3306,
+		'user': 'lk',
+		'password': 'liukai'
+		'database': 'DB_parking_config'
+	},
+	'session':{
+		'secret': 'liukai'
+	}
+}
